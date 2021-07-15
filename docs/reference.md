0619f9df59c12a759573127bb82093e0bb70bbb7
vic.m@mail.ru <Action137>
2021-07-15 19:27:56 +0300

    initial

---
+++ docs/reference.md
@@ -0,0 +1,3 @@
+docker build -f docker/DockerFile .
+docker images
+docker run -p 8080:8080 <image-id>
---
+++ src/main/java/com/epam/demo/HelloController.java
@@ -0,0 +1,18 @@
+package com.epam.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello!!!";
+    }
+
+    @RequestMapping("/hello")
+    public String greeting() {
+        return "Hello Kubernetes!";
+    }
+}
